748e73b2c906572b68942f32b7e4ec878c333ae7
Opiyo24 <gabbynyangor@gmail.com>
2023-07-31 14:17:25 +0300

    islower

---
+++ ALX/If Else Loops/7-islower.py
@@ -0,0 +1,12 @@
+def islower(c):
+    char = ord(c)
+    if char >= 97 and char <= 123:
+        return True
+    else:
+        return False
+
+print("a is {}".format("lower" if islower("a") else "upper"))
+print("H is {}".format("lower" if islower("H") else "upper"))
+print("A is {}".format("lower" if islower("A") else "upper"))
+print("3 is {}".format("lower" if islower("3") else "upper"))
+print("g is {}".format("lower" if islower("g") else "upper"))
