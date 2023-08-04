2e9aa9a83bdb6c1d19af5971f2047bdae62d88ce
Opiyo24 <gabbynyangor@gmail.com>
2023-08-04 05:43:07 +0300

    Formats

--- 0x2-python-import_modules/1-calculation.py
+++ 0x2-python-import_modules/1-calculation.py
@@ -1,6 +1,12 @@
+from calculator_1 import add, sub, mul, div 
 """Imports a function from calculator_1
     does some math
     Prints the rsult"""
 
 a = 10
 b = 5
+
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} - {} = {}".format(a, b, sub(a, b)))
+print("{} * {} = {}".format(a, b, mul(a, b)))
+print("{} / {} = {:.0f}".format(a, b, div(a,b)))
